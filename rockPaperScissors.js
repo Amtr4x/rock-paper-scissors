5a8729248a74cade0b047989f18f8191028ed0b1
leandryscsec <leandryscsec@gmail.com>
2024-07-06 14:24:16 -0400

    Add getComputerChoice function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,3 +1,19 @@
 // @ts-check
 
-console.log("Hello World!");
+/**
+ * Generates a random computer choice from the set ["ROCK", "PAPER", "SCISSORS"].
+ *
+ * @returns {string} The computer's choice, which can be either "ROCK", "PAPER", or "SCISSORS".
+ */
+function getComputerChoice() {
+    const CHOICES = ["ROCK", "PAPER", "SCISSORS"];
+    const RGN = Math.floor(Math.random() * 100);
+
+    if (RGN <= 33) {
+        return CHOICES[0];
+    } else if (RGN <= 66) {
+        return CHOICES[1];
+    } else {
+        return CHOICES[2];
+    }
+}
