3a8cd5b62c167f3f96dca188ff5b5a547d93e96d
leandryscsec <leandryscsec@gmail.com>
2024-07-06 18:35:38 -0400

    Update playRound function and getComputerChoice
    Fix case of the getComputerChoice that was UpperCase and now is LowerCase.
    
    Fix playRound removing the two last extra arguments I added, because they
    are reference values and not modify the original value. Now the counters
    increase successfully.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -6,7 +6,7 @@
  * @returns {string} The computer's choice, which can be either "ROCK", "PAPER", or "SCISSORS".
  */
 function getComputerChoice() {
-    const CHOICES = ["ROCK", "PAPER", "SCISSORS"];
+    const CHOICES = ["rock", "paper", "scissors"];
     const RGN = Math.floor(Math.random() * 100);
 
     if (RGN <= 33) {
@@ -41,10 +41,10 @@ function getHumanChoice() {
         }
     }
 
-    return choice.toLocaleLowerCase();
+    return choice.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice, humanScore, computerScore) {
+function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         console.log("Game drawn!");
     } else if (
